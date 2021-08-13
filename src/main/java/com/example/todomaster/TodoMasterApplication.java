b209564075256efad4545ec2c52ca77f8727083c
qiguang <qiguang0120@163.com>
2021-08-13 12:06:23 +0800

    initial commit

---
+++ src/main/java/com/example/todomaster/TodoMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.todomaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todomaster/TodoMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todomaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
